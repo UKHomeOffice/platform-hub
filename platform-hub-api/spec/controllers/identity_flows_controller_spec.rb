36043b8b6d828869edd1b7a666c971014a67d799
Jits <dev@jits.co.uk>
2017-01-26 15:58:19 +0000

    API: auth tests for the identity flows
    This just checks that authed/unauthed access behaves as expected (and helped catch a bug in the process!)

--- platform-hub-api/spec/controllers/identity_flows_controller_spec.rb
+++ platform-hub-api/spec/controllers/identity_flows_controller_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe IdentityFlowsController, type: :controller do
 
+  describe "GET #start_auth_flow" do
+    it_behaves_like 'unauthenticated not allowed'  do
+      before do
+        get :start_auth_flow, params: { service: 'github' }
+      end
+    end
+  end
+
+  describe "GET #callback" do
+    it 'should allow unauthenticated access but cannot process the request due to a missing param' do
+      get :callback, params: { service: 'github' }
+      expect(response).to have_http_status(422)
+    end
+  end
+
 end
