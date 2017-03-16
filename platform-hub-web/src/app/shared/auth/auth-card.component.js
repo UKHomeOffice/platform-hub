ef5f04dd532e5e18d2129090740e467a21107ecb
Jits <dev@jits.co.uk>
2017-03-16 15:44:02 +0000

    Web: improve the auth status refreshing in the nav menu
    The auth card should now show that the user is not logged in anymore when the session has timed out.

--- platform-hub-web/src/app/shared/auth/auth-card.component.js
+++ platform-hub-web/src/app/shared/auth/auth-card.component.js
@@ -8,23 +8,25 @@ function AuthCardController($scope, $state, authService, events) {
 
   const ctrl = this;
 
-  ctrl.isAuthenticated = false;
   ctrl.authData = {};
 
+  ctrl.isAuthenticated = isAuthenticated;
   ctrl.login = login;
   ctrl.logout = logout;
 
   init();
 
   function init() {
-    ctrl.isAuthenticated = authService.isAuthenticated();
     ctrl.authData = authService.getPayload();
 
     // Listen for further changes
     $scope.$on(events.auth.updated, (event, authData) => {
-      ctrl.isAuthenticated = Boolean(authData);
       ctrl.authData = authData;
     });
+  }
+
+  function isAuthenticated() {
+    return authService.isAuthenticated();
   }
 
   function login() {
