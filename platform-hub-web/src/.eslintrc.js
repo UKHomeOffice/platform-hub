c7faeb294bc76e4b708e21d496d79f13fd730787
Kashif Saadat <kashif.saadat@digital.homeoffice.gov.uk>
2017-06-26 13:47:42 +0100

    [ACPBoard #374] Added unit tests for the loggerService util.

--- platform-hub-web/src/.eslintrc.js
+++ platform-hub-web/src/.eslintrc.js
@@ -2,9 +2,14 @@ module.exports = {
   extends: [
     'angular'
   ],
+  plugins: [
+    'chai-friendly'
+  ],
   rules: {
     'max-params': 0,
     'max-nested-callbacks': ["warn", 10],
-    'angular/no-service-method': 0
+    'angular/no-service-method': 0,
+    "no-unused-expressions": 0,
+    "chai-friendly/no-unused-expressions": 2
   }
 }
