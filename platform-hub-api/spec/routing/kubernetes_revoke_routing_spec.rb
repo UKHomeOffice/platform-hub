867ef2a944f1971fd8a7e0b2eb4f95fe034362ba
Marcin Ciszak <marcin@state.com>
2017-10-10 16:36:35 +0100

    API: Kubernetes token revoke refactor

--- platform-hub-api/spec/routing/kubernetes_revoke_routing_spec.rb
+++ platform-hub-api/spec/routing/kubernetes_revoke_routing_spec.rb
@@ -5,17 +5,17 @@ RSpec.describe Kubernetes::RevokeController, type: :routing do
 
     context 'with kubernetes_tokens feature flag enabled' do
       before do
-	FeatureFlagService.create_or_update(:kubernetes_tokens, true)
+        FeatureFlagService.create_or_update(:kubernetes_tokens, true)
       end
 
       it 'routes to #revoke' do
-	expect(:post => '/kubernetes/revoke').to route_to('kubernetes/revoke#revoke')
+        expect(:post => '/kubernetes/revoke').to route_to('kubernetes/revoke#revoke')
       end
     end
 
     context 'with kubernetes_tokens feature flag disabled' do
       it 'route to #revoke is not routable' do
-	expect(:post => '/kubernetes/revoke').to_not be_routable
+        expect(:post => '/kubernetes/revoke').to_not be_routable
       end
     end
 
