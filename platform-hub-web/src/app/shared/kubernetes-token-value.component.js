8b29d348d1208f94f6818d4e5a7a98c770aeff11
Jits <dev@jits.co.uk>
2018-07-13 14:15:58 +0100

    Web: copy button to copy token value
    … only when you can actually see the whole token value.

--- platform-hub-web/src/app/shared/kubernetes-token-value.component.js
+++ platform-hub-web/src/app/shared/kubernetes-token-value.component.js
@@ -2,5 +2,14 @@ export const KubernetesTokenValueComponent = {
   bindings: {
     item: '<'
   },
-  template: require('./kubernetes-token-value.html')
+  template: require('./kubernetes-token-value.html'),
+  controller: KubernetesTokenValueController
 };
+
+function KubernetesTokenValueController(icons) {
+  'ngInject';
+
+  const ctrl = this;
+
+  ctrl.copyIcon = icons.copy;
+}
