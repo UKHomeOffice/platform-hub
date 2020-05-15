32404de99255be27a3e25cf4b2ccdddf82173eb0
Adam Smith <adam.smith@appvia.io>
2020-05-15 11:23:29 +0100

    debug

--- platform-hub-api/test.sh
+++ platform-hub-api/test.sh
@@ -3,7 +3,7 @@
 # Source local env variables for test execution
 source .env.test
 
-bin/rails db:migrate RAILS_ENV=development
+bin/rails db:migrate RAILS_ENV=test
 
 # Setup the environment
 bin/setup
